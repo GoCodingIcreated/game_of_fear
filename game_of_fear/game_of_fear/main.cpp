828dd312f3363b3c298b25911b7b3993697ab28c
Nickolas <lorddarthveider7@yandex.ru>
2016-06-09 23:15:27 +0300

    new algo

--- game_of_fear/game_of_fear/main.cpp
+++ game_of_fear/game_of_fear/main.cpp
@@ -1,10 +1,20 @@
 #include <windows.h>
 #include <iostream>
+#include <fstream>
 #include <string>
 #include <vector>
 
 
 HWND g_HWND = NULL;
+std::vector<unsigned> allKeys = { 0x41, 0x5A };
+
+template <class T>
+void logger(const T &str) {
+	std::ofstream out("log", std::ios_base::app | std::ios_base::out);
+	out << str << std::endl;
+	out.close();
+}
+
 
 BOOL CALLBACK EnumWindowsProcMy(HWND hwnd, LPARAM lParam)
 {
@@ -18,37 +28,49 @@ BOOL CALLBACK EnumWindowsProcMy(HWND hwnd, LPARAM lParam)
 	return TRUE;
 }
 
-void __stdcall WriteTextInNotepad(const std::string &str)
-{
-	SetForegroundWindow(g_HWND);
-	Sleep(1000);
-	for (size_t i = 0; i < str.size(); ++i) {
-		INPUT input;
-
-		input.type = INPUT_KEYBOARD;
-		input.ki.wScan = input.ki.time = input.ki.dwExtraInfo = 0;
-
-		input.ki.wVk = LOBYTE(VkKeyScan(str[i]));
-		input.ki.dwFlags = 0;
-		SendInput(1, &input, sizeof(input));
-		Sleep(50 + rand() % 300); // небольшая задержка для придания реалистичности
-	}
+BOOL _stdcall IsNothingPressed() {
+	//for (WORD key = 0x41; key <= 0x5A; ++key) {
+		//auto virtual_key = LOBYTE(VkKeyScan(key));
+		//if (GetAsyncKeyState(key) & (1 << 15)) {
+		if (GetAsyncKeyState(VK_END) & 1 << 15) {
+			//logger(key)
+			return FALSE;
+		}
+	//}
+	return TRUE;
 }
 
-// вывод диалога в блокнот с интервалом
-void __stdcall PrintDialog(std::vector<std::string> &dialog) {
-	for (auto sentence : dialog) {
-		WriteTextInNotepad(sentence);
-		Sleep(5000);
-	}
+
+
+BOOL __stdcall PrintLetter(const std::vector<std::wstring> &dialog, int &line, int &pos) {
+	
+	do {
+		if (line >= (int)dialog.size()) {
+			return FALSE;
+		}
+		if (pos >= (int)dialog[line].size()) {
+			line++;
+			pos = 0;
+		}
+	} while (line >= (int)dialog.size() || pos >= (int)dialog[line].size());
+	INPUT input;
+
+	input.type = INPUT_KEYBOARD;
+	input.ki.wScan = input.ki.time = input.ki.dwExtraInfo = 0;
+
+	input.ki.wVk = LOBYTE(VkKeyScan(dialog[line][pos++]));
+	input.ki.dwFlags = 0;
+
+	SendInput(1, &input, sizeof(input));
+	return TRUE;
 }
 
 // инициализируем строки диалога
-void __stdcall InitDialog(std::vector<std::string> &dialog) {
+void __stdcall InitDialog(std::vector<std::wstring> &dialog) {
 	dialog.resize(3);
-	dialog[0] = "Hello, man. What is your name?\n";
-	dialog[1] = "\nDo you like cats?\n";
-	dialog[2] = "\nDo you like dark?\n";
+	dialog[0] = L"Hello, man. What is your name?\n";
+	dialog[1] = L"\nDo you like cats?\n";
+	dialog[2] = L"\nDo you like dark?\n";
 }
 
 void main()
@@ -66,10 +88,25 @@ void main()
 		RECT rect;
 		GetWindowRect(g_HWND, &rect);
 
-		std::vector<std::string> my_dialog;
+		std::vector<std::wstring> my_dialog;
 		InitDialog(my_dialog);
-		PrintDialog(my_dialog);
-
+		//PrintDialog(my_dialog);
+		MSG msg;
+		int line = 0, letter = 0;
+		Sleep(1000);
+		SetForegroundWindow(g_HWND);
+		while (1) {
+			if (IsNothingPressed()) {
+				if (!PrintLetter(my_dialog, line, letter)) {
+					break;
+				}
+				Sleep(50 + rand() % 80); // небольшая задержка для придания реалистичности
+			}
+			else {
+				MessageBox(NULL, L"Dont do it!", L"My caption.", MB_ICONSTOP);
+				SetForegroundWindow(g_HWND);
+			}
+		}
 		Sleep(3000);				// подождать
 		TerminateProcess(pi.hProcess, NO_ERROR);	// убрать процесс
 	}
